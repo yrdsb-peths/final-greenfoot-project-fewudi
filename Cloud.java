db30cdb08bd88da6810dd3727557ee92bf7ab650
iUwUc <130993642+fewudi@users.noreply.github.com>
2023-06-01 08:27:14 -0400

    gdfgag

--- Cloud.java
+++ Cloud.java
@@ -12,6 +12,11 @@ public class Cloud extends Actor
      * Act - do whatever the Cloud wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    public Cloud(){
+        
+    }
+    
     public void act()
     {
         // Add your action code here.
