d342fca399d1a5a16926efa95d28b5788ee8110e
iUwUc <130993642+fewudi@users.noreply.github.com>
2023-06-15 21:35:24 -0400

    sd

---
+++ FirstWorld.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Game meue , music control
+ * 
+ * @Felix P 
+ * @version June 7
+ */
+public class FirstWorld extends World
+{
+
+    /**
+     * Constructor for objects of class FirstWorld.
+     * 
+     */
+    public FirstWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
